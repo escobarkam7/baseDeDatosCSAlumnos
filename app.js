d0550534f6ca61b2b0d6bf700a300ee95a776db6
Kamila <saikimimundo@outlook.es>
2024-02-08 19:32:20 -0600

    Agregar css al index

--- app.js
+++ app.js
@@ -22,10 +22,22 @@ class Alumno {
 
 // 2.- Alta de alumnos
 // Una vez creado el prototipo desde su sitio, deberán dar de alta alumnos, por defecto lo único que deben pedir como requisito al inicio es nombre, apellidos, edad.
+
+
+
+
+
+
+
 // 3.- Después deberas crear funciones y vistas que les ayuden a hacer lo siguiente:
 // Inscribir un alumno a una clase.
 // Asignarle sus calificaciones.
 // Crear grupos y asignarle alumnos (Usen estructuras de datos).
+
+
+
+
+
 // 4.- Ya que tengan sus grupos con alumnos, deberán crear e implementar las siguientes funciones:
 // Buscar por nombre.
 // Buscar por apellido.
